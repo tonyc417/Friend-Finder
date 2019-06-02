498533b93dd3dec01cbdecf2036d625fa66dd4c0
unknown <tonycruz417@gmail.com>
2019-06-02 07:50:51 -0500

    added all questions

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = 8080;
 
+app.get("/suvery", (req,res) => {
+    res.sendFile(__dirname, '')
+});
 
 app.listen(PORT, console.log("Server started on " + PORT));
