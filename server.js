2cae9b62193b682e9991a9fb6f418bc082896127
unknown <tonycruz417@gmail.com>
2019-06-02 14:19:09 -0500

    updated the routes

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ const app = express();
 
 const PORT = 8080;
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/home.html'));
-});
-
-app.get("/survey", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/survey.html'));
-});
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, console.log("Server started on " + PORT));
