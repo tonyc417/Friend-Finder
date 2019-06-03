8aa79a0602a9a4811d4ff4d6c617fe580e0b3fa3
Tony Cruz <tonycruz@Tonys-MacBook-Pro.local>
2019-06-03 17:54:03 -0500

    updated questions

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = (app) => {
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, './public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
     
     app.get("/survey", (req, res) => {
