a7c572d6c6d15ef8aff6a999819843634d2159f9
Tony Cruz <tonycruz@Tonys-MacBook-Pro.local>
2019-06-03 17:43:49 -0500

    working on logic

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+$("#subQuest").on('click', () => {
+    console.log($("#questOne").val());
+    console.log($("#questTwo").val());
+    console.log($("#questThree").val());
+    console.log($("#questFour").val());
+    console.log($("#questFive").val());
+    console.log($("#questSix").val());
+    console.log($("#questSeven").val());
+    console.log($("#questEight").val());
+    console.log($("#questNine").val());
+    console.log($("#questTen").val());
+})
