fcbea12233315005d3d95477a120c71ad431e0d6
unknown <tonycruz417@gmail.com>
2019-06-05 09:23:20 -0500

    working on posting to data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,21 @@
+var results = [{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  ];
+
 $("#subQuest").on('click', () => {
     console.log($("#questOne").val());
     console.log($("#questTwo").val());
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 require("./routing/htmlRoutes")(app);
 
+app.get("/survey", (req, res) => {
+
+})
+
+app.post("/", (req, res) => {
+    
+})
 
 app.listen(PORT, console.log("Server started on " + PORT));
