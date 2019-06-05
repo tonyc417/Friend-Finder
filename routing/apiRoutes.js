32d911ae2fd0b9b5945043599584da8a0e101de1
Tony Cruz <tonycruz@Tonys-MacBook-Pro.local>
2019-06-05 17:43:37 -0500

    sending data is working

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,12 @@
 var friendsData = require("../app/data/friends");
 
-app.get("/api/data", (req, res) => {
-    res.json(friendsData);
-})
+module.exports = (app) => {
+    app.get("/api/data", (req, res) => {
+        res.json(friendsData);
+    })
+    
+    app.post("/api/data", (req, res) => {
+        friendsData.push(req.body);
+        res.json(true);
+    });
+}
