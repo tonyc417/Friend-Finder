f71d931a1c773ff5590ee26954803e870abd7119
unknown <tonycruz417@gmail.com>
2019-06-07 17:26:45 -0500

    working on comparing objects

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-var resultsArr = [{
+var resultsArr = {
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
@@ -13,7 +13,6 @@ var resultsArr = [{
         4,
         1
       ]
-  }
-  ];
+  };
 
 module.exports = resultsArr;
